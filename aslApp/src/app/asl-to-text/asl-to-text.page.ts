45bfe5959e9bd93430e2852d7a20dffd6c5e48f2
myxlinh99 <linhmydo99@yahoo.com.vn>
2020-07-15 11:41:06 -0400

    Camera Added

--- aslApp/src/app/asl-to-text/asl-to-text.page.ts
+++ aslApp/src/app/asl-to-text/asl-to-text.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-asl-to-text',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asl-to-text.page.scss'],
 })
 export class AslToTextPage implements OnInit {
+  imgURL = 'assets/defaultPhoto.png';
 
-  constructor() { }
+  constructor( 
+    private camera: Camera) { }
 
   ngOnInit() {
   }
+  getCamera() {
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      destinationType: this.camera.DestinationType.FILE_URI
+    }).then( (res) => {
+      this.imgURL = res;
+    }).catch (e => {
+      console.log(e);
+    })
 
+
+  }
+
+  getGallery() {
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL
+    }).then( (res) => {
+      this.imgURL = 'data:image/jpeg;base64,' + res;
+    }).catch (e => {
+      console.log(e);
+    })
+
+  }
 }
